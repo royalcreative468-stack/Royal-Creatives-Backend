e9e7aa9005b10cba10216cf01b50cac9b4200849
unknown <royalcreative468@gmail.com>
2025-10-05 21:58:52 +0530

    cors url updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://royal-creatives.vercel.app/',
+    origin:'https://royal-creatives.vercel.app',
     credentials:true
 }
 
